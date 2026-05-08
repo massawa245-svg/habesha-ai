356078bbea80d71749a504de36b5ede1b28eb933
massawa245-svg <massawa245@gmail.com>
2026-05-08 20:20:52 +0200

    fix: remove invalid turbo experimental config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.152.14.133'],
-  experimental: {
-    turbo: {
-      // Turbopack Konfiguration
-    }
-  }
 };
 
 export default nextConfig;
